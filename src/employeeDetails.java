52c7fc4c2f0625096e88d8cda1cdd974f1b7083e
Gokul Das M <Gokul@NTP-LAP-723.chn.nestgroup.net>
2022-11-21 16:31:46 +0530

    employee details added

---
+++ src/employeeDetails.java
@@ -0,0 +1,19 @@
+public class employeeDetails {
+    int empId;
+    String name,designation,companyName;
+    double salary;
+    long phoneNumber;
+    String mailId;
+
+    public employeeDetails(int empId,String name,String designation,String companyName,double salary,long phoneNumber){
+        this.empId=empId;
+        this.name=name;
+        this.designation=designation;
+        this.companyName=companyName;
+        this.salary=salary;
+        this.phoneNumber=phoneNumber;
+    }
+
+
+
+}
